74b1c1e07348bcfb27e6f79e79757bed80a99af2
Dan Ford <daniel.ford@libertymutual.com>
2017-07-11 10:48:05 -0400

    added built-with-react plus atom image and link to facebook react githum in the footer

--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -2,14 +2,19 @@ import React, { Component } from 'react';
 
     import './Footer.css';
 
+    import travelIcon from "../assets/images/react-icon.png";
+
     class Footer extends Component {
      render() {
         return (
             <div id="footer">&copy; My Travel Site - Travel is fun!
                 <div>
-                    <a href="http://facebook.com">Facebook</a>
+                    <label>Built with React &nbsp;
+                        <img src={travelIcon} width="45px" height="45px" alt="react icon"></img>
+                    </label>
+                     &nbsp;/&nbsp;
+                    <a href="https://facebook.github.io/react/">Facebook Github IO</a>
                     &nbsp;/&nbsp;
-                    <a href="http://twitter.com">Twitter</a>
                 </div>
             </div>
         );
